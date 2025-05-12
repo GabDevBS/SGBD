2a783aaed4d8ae0d25b25a57838f7ee39e16808a
Gabriel Sousa <gabriel1908bs@gmail.com>
2025-05-12 09:46:52 -0300

    First Commit

---
+++ db.js
@@ -0,0 +1,33 @@
+const sqlite = require('sqlite3');
+const { open } = require('sqlite')
+
+// criar uma conexão com o banco de dados
+async function main() {
+    try{
+        const db = await open({
+        filename: './banco.db',
+        driver: sqlite.Database
+    })
+    await db.exec(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE
+        )`)
+
+    await db.run(`INSERT INTO users (nome, email) 
+    VALUES (?, ?)`, ['Gabriel', 'gabriel_b_sousa2@estudante.sesisenai.org.br'])
+
+        const usuarios = await db.all(`SELECT * FROM users`)
+        console.log(usuarios)
+
+    await db.close()
+    } catch (err) {
+        console.log(err)
+    }
+}
+main()
+
+// executar um script simples de criação de tabela
+// executar um script simples adicionando um usuário
+// executar um script simples de leitura na tabela
+// encerrar a conexão 
